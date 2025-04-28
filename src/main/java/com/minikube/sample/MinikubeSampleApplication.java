51df5bec1392716c69311207c08b7f633cd3bd71
ravigupta <126137635+ravi1003@users.noreply.github.com>
2025-04-28 16:01:59 +0530

    Update MinikubeSampleApplication.java

--- src/main/java/com/minikube/sample/MinikubeSampleApplication.java
+++ src/main/java/com/minikube/sample/MinikubeSampleApplication.java
@@ -1,15 +1,39 @@
+// package com.minikube.sample;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// @EnableAutoConfiguration
+// public class MinikubeSampleApplication {
+
+//     public static void main(String[] args) {
+
+//         SpringApplication.run(MinikubeSampleApplication.class, args);
+//     }
+// }
+
+
 package com.minikube.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@RestController
 public class MinikubeSampleApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(MinikubeSampleApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Dockerized Spring Boot App!";
     }
 }
