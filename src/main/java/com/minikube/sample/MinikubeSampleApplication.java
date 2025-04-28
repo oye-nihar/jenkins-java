25462fcdf10e5ee8bd8bd25477c60475adb0406b
ravigupta <126137635+ravi1003@users.noreply.github.com>
2025-04-28 17:20:09 +0530

    Update MinikubeSampleApplication.java

--- src/main/java/com/minikube/sample/MinikubeSampleApplication.java
+++ src/main/java/com/minikube/sample/MinikubeSampleApplication.java
@@ -1,42 +1,42 @@
+package com.minikube.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MinikubeSampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MinikubeSampleApplication.class, args);
+    }
+}
+
+
 // package com.minikube.sample;
 
 // import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 // import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 // @SpringBootApplication
 // @EnableAutoConfiguration
+// @RestController
 // public class MinikubeSampleApplication {
 
 //     public static void main(String[] args) {
-
 //         SpringApplication.run(MinikubeSampleApplication.class, args);
+//     }
+
+//     @GetMapping("/")
+//     public String home() {
+//         return "Hello from Dockerized Spring Boot App!";
 //     }
 // }
 
-
-package com.minikube.sample;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class MinikubeSampleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(MinikubeSampleApplication.class);
-        application.setDefaultProperties(
-            java.util.Collections.singletonMap("server.port", "8092"));  // Change the port to 8090
-        application.run(args);
-    }
-
-    @GetMapping("/")
-    String home() {
-        return "Hello from Dockerized Spring Boot App!";
-    }
-}
 
 
