ae7937c4c6e377e3942530f31ced5321c1c99302
ravigupta <126137635+ravi1003@users.noreply.github.com>
2025-05-04 17:37:51 +0530

    Update MinikubeSampleApplication.java

--- src/main/java/com/minikube/sample/MinikubeSampleApplication.java
+++ src/main/java/com/minikube/sample/MinikubeSampleApplication.java
@@ -18,6 +18,6 @@ public class MinikubeSampleApplication {
     // GET endpoint for "/"
     @GetMapping("/")
     public String hello() {
-        return "Happy Learning!";
+        return "Devops is Cool!";
     }
 }
